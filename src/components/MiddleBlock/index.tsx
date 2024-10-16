4a0047886cd26e64c4629cdc8e37eda5a59ff5fc
Hipol1to <hipolitoprz2001@gmail.com>
2024-10-15 21:08:54 -0400

    modal aligned

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -140,51 +140,46 @@ const MiddleBlock = ({ title, content, button, t }: MiddleBlockProps) => {
       {isOpen && (
         <ModalOverlay>
           <ModalContainer>
+            <CloseButton onClick={closeModal}>×</CloseButton>
             <ContactContainer id={"invest-container"}>
               <Row justify="space-between" align="middle">
-                <Col lg={12} md={11} sm={24} xs={24}>
-                  <Slide direction="left" triggerOnce>
-                    <Block title={title} content={content} />
-                  </Slide>
-                </Col>
-                <Col lg={12} md={12} sm={24} xs={24}>
-                  <Slide direction="right" triggerOnce>
-                    <FormGroup autoComplete="off" onSubmit={handleSubmit}>
-                      <Col span={24}>
-                        <Input
-                          type="text"
-                          name="Nombre"
-                          placeholder="Tu nombre"
-                          value={values.name || ""}
-                          onChange={handleChange}
-                        />
-                        <ValidationType type="name" />
-                      </Col>
-                      <Col span={24}>
-                        <Input
-                          type="text"
-                          name="Correo electronico"
-                          placeholder="Tu correo electronico"
-                          value={values.email || ""}
-                          onChange={handleChange}
-                        />
-                        <ValidationType type="email" />
-                      </Col>
-                      <Col span={24}>
-                        <TextArea
-                          placeholder="Tu mensaje"
-                          value={values.message || ""}
-                          name="mensaje"
-                          onChange={handleChange}
-                        />
-                        <ValidationType type="message" />
-                      </Col>
-                      <ButtonContainer>
-                        <Button name="submit">{t("Enviar")}</Button>
-                      </ButtonContainer>
-                    </FormGroup>
-                  </Slide>
-                </Col>
+                <Slide direction="right" triggerOnce>
+                  <FormGroup autoComplete="off" onSubmit={handleSubmit}>
+                    <Col span={24}>
+                      <Block title={title} content={"content"} />
+                      <Input
+                        type="text"
+                        name="Nombre"
+                        placeholder="Tu nombre"
+                        value={values.name || ""}
+                        onChange={handleChange}
+                      />
+                      <ValidationType type="name" />
+                    </Col>
+                    <Col span={24}>
+                      <Input
+                        type="text"
+                        name="Correo electronico"
+                        placeholder="Tu correo electronico"
+                        value={values.email || ""}
+                        onChange={handleChange}
+                      />
+                      <ValidationType type="email" />
+                    </Col>
+                    <Col span={24}>
+                      <TextArea
+                        placeholder="Tu mensaje"
+                        value={values.message || ""}
+                        name="mensaje"
+                        onChange={handleChange}
+                      />
+                      <ValidationType type="message" />
+                    </Col>
+                    <ButtonContainer>
+                      <Button name="submit">{t("Enviar")}</Button>
+                    </ButtonContainer>
+                  </FormGroup>
+                </Slide>
               </Row>
             </ContactContainer>
           </ModalContainer>
